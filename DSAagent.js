764ed6a577ded19b53a600aae56e77ccd46af4db
Rohit Singh <rohitsingh94874@gmail.com>
2025-07-16 08:45:37 +0530

    first commit

---
+++ DSAagent.js
@@ -0,0 +1,31 @@
+
+// import  {GoogleGenAI}  from "@google/genai";
+// import readlineSync from "readline-sync";
+
+// const ai = new GoogleGenAI({apiKey: "AIzaSyAcoQdNrO2bp8diUN9jiowlDn_r6hdTQOk"});
+
+//     const chat = ai.chats.create({
+//     model: "gemini-2.5-flash",
+//      config: {
+//       systemInstruction: "You are a ai agent which can only ans for data structures and algorithms related questions, if anyone ask which is not related to dsa reply them rudely ",
+//     },
+//     history: []
+//   });
+
+
+// async function main(){
+
+//     var userProblem = readlineSync.question('Ask me Anything..... ');
+
+//     const response1 = await chat.sendMessage({
+//     message: userProblem,
+//    });
+
+//     console.log( response1.text);
+
+//     main();
+// } 
+
+// await main();
+
+
